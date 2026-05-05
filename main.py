d549022e8b238ec0872dd0228ece40dc44b94abb
anishpandit <anishasok@gmail.com>
2026-05-06 00:12:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from sqlalchemy import create_engine, text
+import os
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "HireAnEmployee API is live 🚀"}
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+
+
+@app.get("/db-check")
+def db_check():
+    with engine.connect() as conn:
+        conn.execute(text("SELECT 1"))
+    return {"status": "connected"}
